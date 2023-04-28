0c473335e80b95d32cf9157a65428431a0ed4247
keenlabi <keenlabinc@gmail.com>
2023-04-28 04:39:13 +0100

    feat(staff list page): created new page for staff list

---
+++ src/pages/Dashboard/Staffs/StaffList/StaffList.tsx
@@ -0,0 +1,9 @@
+import styles from "./stafflist.module.css";
+
+export default function StaffList() {
+    return (
+        <div className={styles.staff_list}>
+            
+        </div>
+    )
+}
