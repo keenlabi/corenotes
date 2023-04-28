3a622e9aa5b07d545cae7ba3328e4ce05b1a6184
keenlabi <keenlabinc@gmail.com>
2023-04-28 02:10:20 +0100

    feat(dashboard routes): added new staff route as a child of '/dashboard' route -> '/dashboard/staff'

--- src/routes/public/dashboard/dashboardRoutes.tsx
+++ src/routes/public/dashboard/dashboardRoutes.tsx
@@ -1,12 +1,20 @@
 import { routerType } from "src/routes/types";
 import Dashboard from "src/pages/Dashboard";
+import Staffs from "src/pages/Dashboard/Staffs";
 
 const DashboardRoutes:routerType[] = [
     {
         path:'/dashboard',
         title: 'Dashboard',
-        element: <Dashboard />
-    }
+        element: <Dashboard />,
+        children: [
+            {
+                path:'staffs',
+                title: 'Staffs',
+                element: <Staffs />
+            }
+        ]
+    },
 ]
 
 export default DashboardRoutes;
