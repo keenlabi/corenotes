edf612d980f931b8ffcd669fd43984d3c1f11b8d
keenlabi <keenlabinc@gmail.com>
2023-04-29 06:25:47 +0100

    feat(staff list): created dummy staff list data as placeholders in the StaffListTable component

--- src/pages/Dashboard/Staffs/StaffList/StaffList.tsx
+++ src/pages/Dashboard/Staffs/StaffList/StaffList.tsx
@@ -1,9 +1,36 @@
+import StaffListTable from "./StaffListTable";
 import styles from "./stafflist.module.css";
+import profilePicture from "src/assets/images/user-dp.png"
 
 export default function StaffList() {
+
+    const staffList = [
+        {
+            id:'1',
+            profilePicture: profilePicture,
+            fullname: 'Williams, Augusta',
+            role: 'Super admin',
+            cellPhone: '(678)123-1234',
+            compartment: 'test compartment'
+        },
+        {
+            id:'2',
+            profilePicture: profilePicture,
+            fullname: 'Williams, Augusta',
+            role: 'Super admin',
+            cellPhone: '(678)123-1234',
+            compartment: 'test compartment'
+        }
+    ]
+
     return (
         <div className={styles.staff_list}>
-            
+            <StaffListTable 
+                staffs={staffList}
+                currentPage={0} 
+                totalPages={0} 
+                goToPage={(pageNumber:number)=> console.log(pageNumber)} 
+            />
         </div>
     )
 }
