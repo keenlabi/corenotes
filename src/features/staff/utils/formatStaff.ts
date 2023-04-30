123cca26aaea36ca37fca4eccace57de75acb4d5
keenlabi <keenlabinc@gmail.com>
2023-04-30 12:42:35 +0100

    feat(staff state utilities): created new formatStaff utitlity function

---
+++ src/features/staff/utils/formatStaff.ts
@@ -0,0 +1,12 @@
+import { IUser } from "../types"
+
+export interface staffDetailsType extends Omit<IUser, '_id'> {
+    id: string
+}
+
+export default function formatStaffList(staff:IUser):staffDetailsType {
+    return {
+        ...staff,
+        id: staff._id
+    }
+}
