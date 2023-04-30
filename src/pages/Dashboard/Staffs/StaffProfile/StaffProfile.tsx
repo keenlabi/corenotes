5f3b428ad35f3fc610357626a640b099c6c917ea
keenlabi <keenlabinc@gmail.com>
2023-04-30 09:04:21 +0100

    feat(staff profile): created new page to display staff profile

---
+++ src/pages/Dashboard/Staffs/StaffProfile/StaffProfile.tsx
@@ -0,0 +1,9 @@
+import styles from "./staffprofile.module.css";
+
+export default function StaffProfile() {
+    return (
+        <div className={styles.staff_profile}>
+            
+        </div>
+    )
+}
