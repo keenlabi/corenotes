d8a9355350990c3ba9185f2aad4a4aca4d51b1a6
keenlabi <keenlabinc@gmail.com>
2023-04-30 12:08:43 +0100

    feat(staff profile routes): created new staff profile nested routes with the first item directing to the staff profile information

--- src/routes/dashboard/dashboardRoutes.tsx
+++ src/routes/dashboard/dashboardRoutes.tsx
@@ -3,7 +3,6 @@ import Dashboard from "src/pages/Dashboard";
 import Staffs from "src/pages/Dashboard/Staffs";
 import StaffList from "src/pages/Dashboard/Staffs/StaffList";
 import StaffProfile from "src/pages/Dashboard/Staffs/StaffProfile";
-import StaffPersonalInformation from "src/pages/Dashboard/Staffs/StaffProfile/StaffProfileInformation/StaffPersonalInformation";
 import StaffProfileInformation from "src/pages/Dashboard/Staffs/StaffProfile/StaffProfileInformation";
 
 const DashboardRoutes:routerType[] = [
