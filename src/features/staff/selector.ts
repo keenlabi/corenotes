32caf2c975e12057c810acb3ab75c2bc49ac2086
keenlabi <keenlabinc@gmail.com>
2023-05-05 21:02:14 +0100

    feat(staff state selector): created seletor to fetch staff document using the fetchStaffDocumentsAction

--- src/features/staff/selector.ts
+++ src/features/staff/selector.ts
@@ -1,8 +1,9 @@
 import { selectorFamily, useRecoilValue } from "recoil";
-import {fetchStaffAction, fetchStaffListAction, fetchStaffListSuccessResponseType, fetchStaffSuccessResponseType } from "./actions";
+import {fetchStaffAction, fetchStaffDocumentsAction, fetchStaffDocumentsSuccessResponseType, fetchStaffListAction, fetchStaffListSuccessResponseType, fetchStaffSuccessResponseType } from "./actions";
 import formatStaffList from "./utils/formatStaffsList";
 import formatStaff from "./utils/formatStaff";
 import { staffState } from "./state";
+import formatStaffDocumentsList from "./utils/formatStaffDocuments";
 
 const fetchStaffsListSelector = selectorFamily({
     key: 'fetch_staffs_list_selector',
@@ -54,3 +55,33 @@ const fetchStaffSelector = selectorFamily({
 })
 
 export const useFetchStaffSelector = (id:string)=> useRecoilValue(fetchStaffSelector(id))
+
+const fetchStaffDocumentsSelector = selectorFamily({
+    key: 'fetch_staff_documents_selector',
+    get: ({id, pageNumber}:{id:string, pageNumber:number})=> async ()=> {
+        return await fetchStaffDocumentsAction(id, pageNumber)
+        .then(({ data }:fetchStaffDocumentsSuccessResponseType)=> {
+            return {
+                documents: formatStaffDocumentsList(data.documents),
+                currentPage: data.currentPage,
+                totalPages: data.totalPages,
+                code: 200,
+                message: '',
+                error: false
+            }
+        })
+        .catch((error)=> {
+            console.log(error)
+            return {
+                code: 500,
+                error: true,
+                message: error.response.message,
+                documents: [],
+                currentPage: 0,
+                totalPages: 0,
+            }
+        })
+    }
+})
+
+export const useFetchStaffDocumentsSelector = (id:string, pageNumber:number)=> useRecoilValue(fetchStaffDocumentsSelector({id, pageNumber}))
