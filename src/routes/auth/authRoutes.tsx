b0dc19b358f8f9e19331be9d6a7f963e49f2326e
keenlabi <keenlabinc@gmail.com>
2023-05-06 06:04:55 +0100

    fix(auth routes): added the login route again

--- src/routes/auth/authRoutes.tsx
+++ src/routes/auth/authRoutes.tsx
@@ -1,11 +1,12 @@
+import Login from "src/pages/Auth/Login";
 import { routerType } from "src/routes/types";
 
 const AuthRoutes:routerType[] = [
-    // {
-    //     path:'',
-    //     title: 'Login',
-    //     element: <Login />
-    // }
+    {
+        path:'/',
+        title: 'Login',
+        element: <Login />
+    }
 ]
 
 export default AuthRoutes;
