999fac9c72abd1b411a8d23cb1d7391f39e844a9
keenlabi <keenlabinc@gmail.com>
2023-05-06 17:23:06 +0100

    feat(date range field components): created new date range field to quickly get a start and end date range

---
+++ src/components/FormComponents/DateRangeField/DateRangeField.tsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+import { formFieldType, setFormFieldType } from "../FormWrapper/types";
+import InputField from "../InputField";
+import styles from "./daterangefield.module.css";
+import {ReactComponent as IconCalendar } from "src/assets/icons/icon-calendar.svg"
+
+export default function DateRangeField({
+    onSelect
+}:{
+    defaultStart?:string,
+    defaultEnd?:string,
+    onSelect:({start, end}:{start:string, end:string})=> void
+}) {
+
+    const [startDateModel, setStartDateModel] = useState<formFieldType>({
+        type:'date',
+        label:'Start date',
+        placeholder:'Start date',
+        value:'',
+        error: '',
+        validated: false
+    })
+
+    const [endDateModel, setEndDateModel] = useState<formFieldType>({
+        type:'date',
+        label:'End date',
+        placeholder:'End date',
+        value:'',
+        error: '',
+        validated: false
+    })
+
+    function setInput(value:string, inputModel:formFieldType, setInputModel:setFormFieldType) {
+        inputModel.value = value
+        validateModel(inputModel)
+        setInputModel({...inputModel});
+
+        setRange()
+    }
+
+    function validateModel(updatedInputModel:formFieldType) {
+        if(!updatedInputModel.value) {
+            updatedInputModel.validated = false;
+            return
+        }
+
+        updatedInputModel.validated = true;
+        return
+    }
+
+    function setRange() {
+        if(startDateModel.validated && endDateModel.validated) {
+            onSelect({
+                start: startDateModel.value,
+                end: endDateModel.value
+            })
+        }
+    }
+
+    return (
+        <div className={styles.date_range_field}>
+            <InputField 
+                inputWidth={'100%'}
+                inputContainer={'150px'}
+                type={startDateModel.type}
+                placeholder={startDateModel.placeholder}
+                value={startDateModel.value}
+                error={startDateModel.error}
+                onInput={(value: string) => setInput(value, startDateModel, setStartDateModel)} 
+            />
+
+            <InputField 
+                inputWidth={'100%'}
+                inputContainer={'150px'}
+                type={endDateModel.type}
+                placeholder={endDateModel.placeholder}
+                value={endDateModel.value}
+                error={endDateModel.error}
+                onInput={(value: string) => setInput(value, endDateModel, setEndDateModel)} 
+            />
+        </div>
+    )
+}
