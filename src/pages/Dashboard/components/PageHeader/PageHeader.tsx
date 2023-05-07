0779aad49223e9a1aa777f993f0ecbdc0e7bf1fc
keenlabi <keenlabinc@gmail.com>
2023-05-07 09:11:36 +0100

    feat(dashboard components): styled the absolute search bar; replaced the dummy data of user profile card with authenticated user data

--- src/pages/Dashboard/components/PageHeader/PageHeader.tsx
+++ src/pages/Dashboard/components/PageHeader/PageHeader.tsx
@@ -1,11 +1,49 @@
+import InputField from "src/components/FormComponents/InputField"
 import UserProfileCard from "../UserProfileCard"
 import styles from "./pageheader.module.css"
+import { useState } from "react"
+import { formFieldType, setFormFieldType } from "src/components/FormComponents/FormWrapper/types"
+import { ReactComponent as IconSearch } from "src/assets/icons/icon-search.svg"
 
 export default function PageHeader() {
+
+    const [searchModel, setSearchModel] = useState<formFieldType>({
+        type:'text',
+        placeholder:'Search by keyword',
+        prefixIcon: <IconSearch />,
+        value: '',
+        error:'',
+        validated: false
+    })
+
+    function setInput(value:string, inputModel:formFieldType, setInputModel:setFormFieldType) {
+        inputModel.value = value
+        validateModel(inputModel)
+        setInputModel({...inputModel});
+    }
+
+    function validateModel(updatedInputModel:formFieldType) {
+        if(!updatedInputModel.value) {
+            updatedInputModel.validated = false;
+            return
+        }
+
+        updatedInputModel.validated = true;
+        return
+    }
+
     return (
         <div className={styles.page_header}>
             <div className={styles.search_bar}>
-
+                <InputField
+                    extraInputContainerStyle={styles.input_container}
+                    type={searchModel.type}
+                    value={searchModel.value} 
+                    placeholder={searchModel.placeholder}
+                    prefixIcon={searchModel.prefixIcon}
+                    error={""}
+                    onInput={(value:string)=> setInput(value, searchModel, setSearchModel)}  
+                />
             </div>
 
             <UserProfileCard
