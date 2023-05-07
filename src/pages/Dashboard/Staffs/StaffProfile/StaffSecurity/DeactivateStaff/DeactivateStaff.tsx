45d52dafd521d108161f2a3b5d594bfa64c3e7b0
keenlabi <keenlabinc@gmail.com>
2023-05-07 07:06:50 +0100

    form(de/activate staff logic):  enable button to trigger the action function

--- src/pages/Dashboard/Staffs/StaffProfile/StaffSecurity/DeactivateStaff/DeactivateStaff.tsx
+++ src/pages/Dashboard/Staffs/StaffProfile/StaffSecurity/DeactivateStaff/DeactivateStaff.tsx
@@ -1,11 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./deactivatestaff.module.css";
 import { formFieldType, setFormFieldType } from "src/components/FormComponents/FormWrapper/types";
 import PasswordInputField from "src/components/FormComponents/InputField/PasswordInputField";
 import FadedBackgroundButton from "src/components/Buttons/FadedBackgroundButton";
+import { useParams } from "react-router-dom";
+import { activateStaffPasswordAction, deactivateStaffPasswordAction, fetchStaffSuccessResponseType } from "src/features/staff/actions";
+import { useStaffState } from "src/features/staff/state";
+import FormWrapper from "src/components/FormComponents/FormWrapper";
+import formatStaff from "src/features/staff/utils/formatStaff";
+import PrimaryTextButton from "src/components/Buttons/PrimaryTextButton";
 
 export default function DeactivateStaff() {
     
+    const { id } = useParams();
+
+    const [staffState, setStaffState] = useStaffState()
+    const [activateStaffState, setActivateStaffState] = useState(staffState)
+
+    const [isStaffActive, setIsStaffActive] = useState(staffState.details.active)
+
     const [userPassword, setUserPassword] = useState<formFieldType>({
         type:'password',
         label:'Enter password',
@@ -52,17 +65,109 @@ export default function DeactivateStaff() {
             password: userPassword.value
         }
 
-        console.log(payload)
+        setActivateStaffState(state => {
+            return {
+                ...state,
+                error: false,
+                status: 'LOADING',
+                message: ''
+            }
+        })
+
+        deactivateStaffPasswordAction(id!, payload)
+        .then((response:fetchStaffSuccessResponseType)=> {
+            setActivateStaffState(state => {
+                return {
+                    ...state,
+                    details: formatStaff(response.data.staff),
+                    error: false,
+                    status: 'SUCCESS',
+                    message: response.message
+                }
+            })
+
+            setStaffState(state => {
+                return {
+                    ...state,
+                    details: formatStaff(response.data.staff),
+                    error: false,
+                    status: 'SUCCESS'
+                }
+            })
+        })
+        .catch((error)=> {
+            setActivateStaffState(state => {
+                return {
+                    ...state,
+                    error: true,
+                    status: 'FAILED',
+                    message: error.message
+                }
+            })
+        })
+    }
+
+    function activateStaff() {
+        const payload = {
+            password: userPassword.value
+        }
+
+        setActivateStaffState(state => {
+            return {
+                ...state,
+                error: false,
+                status: 'LOADING',
+                message: ''
+            }
+        })
+
+        activateStaffPasswordAction(id!, payload)
+        .then((response:fetchStaffSuccessResponseType)=> {
+            setActivateStaffState(state => {
+                return {
+                    ...state,
+                    error: false,
+                    status: 'SUCCESS',
+                    message: response.message
+                }
+            })
+
+            setStaffState(state => {
+                return {
+                    ...state,
+                    details: {
+                        ...state.details,
+                        active: response.data.staff.active
+                    }
+                }
+            })
+        })
+        .catch((error)=> {
+            setActivateStaffState(state => {
+                return {
+                    ...state,
+                    error: true,
+                    status: 'FAILED',
+                    message: error.message
+                }
+            })
+        })
     }
     
     return (
         <div className={styles.account_reset}>
             <div className={styles.left}>
-                <div className={styles.heading}>Account reset</div>
-                <div className={styles.sub_heading}>Manage staff accounts</div>
+                <div className={styles.heading}>Deactivate staff</div>
+                <div className={styles.sub_heading}>
+                    Enter your admin password to activate staff
+                </div>
             </div>
 
-            <div className={styles.right}>
+            <FormWrapper 
+                state={activateStaffState}
+                resetState={()=> setActivateStaffState({...activateStaffState, status: 'IDLE', error:false})}
+                extraStyles={styles.right}
+            >
                 <PasswordInputField 
                     placeholder={userPassword.placeholder}
                     value={userPassword.value}
@@ -70,14 +175,24 @@ export default function DeactivateStaff() {
                     onInput={(value: string) => setInput(value, userPassword, setUserPassword)} 
                 />
 
-                <FadedBackgroundButton
-                    label="Deactivate staff"
-                    disabled={!isFormValid}
-                    backgroundColor={"var(--red-accent-faded-100)"}
-                    labelColor={"var(--red-accent-100)"}
-                    action={()=> deactivateStaff()}
-                />
-            </div>
+                {   
+                    isStaffActive
+                    ?   <FadedBackgroundButton
+                            label="Deactivate staff"
+                            disabled={!isFormValid}
+                            backgroundColor={"var(--red-accent-faded-100)"}
+                            labelColor={"var(--red-accent-100)"}
+                            isLoading={activateStaffState.status === 'LOADING'}
+                            action={()=> deactivateStaff()}
+                        />
+                    :   <PrimaryTextButton 
+                            label="Activate staff"
+                            disabled={!isFormValid}
+                            isLoading={activateStaffState.status === 'LOADING'}
+                            clickAction={()=> activateStaff()}
+                        />
+                }
+            </FormWrapper>
         </div>
     )
 }
