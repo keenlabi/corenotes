23f6ca46d96a22965f858f9788d3921b3f0fdb94
keenlabi <keenlabinc@gmail.com>
2023-06-03 15:29:19 +0100

    feat(layout components): added new contentContainerWidth to the modal container to adjust the width of the content container from the parent component

--- src/components/Modal/ModalContainer/ModalContainer.tsx
+++ src/components/Modal/ModalContainer/ModalContainer.tsx
@@ -1,11 +1,15 @@
 import styles from "./modalcontainer.module.css";
 
-export default function ModalContainer({children, close}:{children:JSX.Element, close:()=> void}) {
+export default function ModalContainer({
+    children, 
+    close,
+    contentContainerWidth
+}:{children:JSX.Element, close:()=> void, contentContainerWidth?:string}) {
     return (
         <div className={styles.modal_container}>
             <div className={styles.modal_bg} onClick={()=> close()}></div>
             
-            <div className={styles.content_container}>
+            <div className={styles.content_container} style={{width:contentContainerWidth}}>
                 { children }
             </div>
         </div>
