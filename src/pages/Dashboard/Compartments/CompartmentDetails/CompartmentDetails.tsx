6102e0dd5717057978da3609f6812b8809f4bd3b
Keenlabi <keenlabinc@gmail.com>
2023-06-28 03:51:52 +0100

    feat(compartment details screen): created the page for compartment details; added the path props to compartment card to navigate to compartment details page.

---
+++ src/pages/Dashboard/Compartments/CompartmentDetails/CompartmentDetails.tsx
@@ -0,0 +1,44 @@
+import { useNavigate, useParams } from "react-router-dom";
+import styles from "./compartmentdetails.module.css";
+import { FaAngleLeft } from "react-icons/fa";
+import capitalize from "src/utils/capitalize";
+import { useEffect } from "react";
+import { useCompartmentState } from "src/features/compartment/state";
+import { useFetchCompartmentDetails } from "src/features/compartment/selector";
+
+export default function CompartmentDetails() {
+
+    const { compartmentId } = useParams();
+
+    const navigate = useNavigate()
+
+    const [compartmentState, setCompartmentState] = useCompartmentState();
+
+    const compartmentDetailsResponse = useFetchCompartmentDetails(parseInt(compartmentId!));
+
+    useEffect(()=> {
+        setCompartmentState(state => ({
+            ...state,
+            error: compartmentDetailsResponse.error,
+            message: compartmentDetailsResponse.message,
+            compartment: compartmentDetailsResponse.compartment,
+        }))
+
+    }, [compartmentDetailsResponse, setCompartmentState])
+
+    return (
+        <div className={styles.compartment_details}> 
+            <div className={styles.header}>
+                <FaAngleLeft 
+                    className={styles.back_btn} 
+                    onClick={()=> navigate({ pathname: '/dashboard/compartments' }) }
+                />
+
+                <div className={styles.heading}>{ capitalize(compartmentState.compartment.title) }</div>
+            </div>
+            {/* display component title */}
+            {/* display attached services */}
+            {/* display individuals */}
+        </div>
+    )
+}
